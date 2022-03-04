22c2a8519e970feb83f1e0b16e3cb91d548a1563
umitobird <umitobird@gmail.com>
2022-03-04 15:34:29 +0800

    NG

---
+++ src/main/java/Test/ScoringNG.java
@@ -0,0 +1,12 @@
+package Test;
+
+public class ScoringNG {
+    public static void main(String[] args) {
+        String [] name = {"A","B","C"};
+        int [] English = {10,20,30};
+        for (int i = 0; i <3; i++) {
+            if (i!=2)
+            System.out.println(name[i]+"\t"+English[i]);
+        }
+    }
+}
