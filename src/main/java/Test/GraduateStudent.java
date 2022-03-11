f24b76eb4c4f9cd092850bb25c63904b06dcec58
umitobird <umitobird@gmail.com>
2022-03-11 16:25:43 +0800

    0311

---
+++ src/main/java/Test/GraduateStudent.java
@@ -0,0 +1,15 @@
+package Test;
+
+public class GraduateStudent extends Student{
+    int thesis;
+    public GraduateStudent(String name, int english,
+                           int math, int thesis){
+        super(name, english, math);
+        this.thesis = thesis;
+    }
+    public void print(){
+        System.out.println(name + "\t" + english
+                + "\t" + math + "\t"
+                + getAverage() + thesis);
+    }
+}
--- src/main/java/Test/Scoring.java
+++ src/main/java/Test/Scoring.java
@@ -2,9 +2,15 @@ package Test;
 
 public class Scoring {
     public static void main(String[] args) {
-        Student A = new Student();
-        A.name ="Jack";
-        A.english = 70;
-        A.math = 88;
+        Student jack = new Student("Jack");
+        jack.english = 70;
+        jack.math = 88;
+        Student Hank = new Student("Hank",60,50);
+        Student s = new Student();
+        GraduateStudent jane = new GraduateStudent("Jane",65,82,79);
+
+        jane.print();
+        jack.print();
+        Hank.print();
     }
 }
