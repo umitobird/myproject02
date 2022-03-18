d1dcf625509626ea242e38bbb994f62fe0e7ad96
umitobird <umitobird@gmail.com>
2022-03-18 16:13:12 +0800

    0318b

---
+++ src/main/java/Test/GoldenCustomer.java
@@ -0,0 +1,9 @@
+package Test;
+
+public class GoldenCustomer extends SilverCustomer {
+
+    public GoldenCustomer(String id, int amount) {
+        super(id, amount);
+        float off = 0.2f;
+    }
+}
--- src/main/java/Test/Saletaster.java
+++ src/main/java/Test/Saletaster.java
@@ -5,8 +5,10 @@ public class Saletaster {
         Customer c1 = new Customer("0001", 1200);
         Customer c2 = new Customer("0002", 800);
         SilverCustomer c3 = new SilverCustomer("0003", 2000);
+        GoldenCustomer c4 = new GoldenCustomer("0004", 4000);
         c1.print();
         c2.print();
         c3.print();
+        c4.print();
     }
 }
