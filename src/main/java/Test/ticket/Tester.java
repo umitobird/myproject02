6fe863886bbc1ff2385b2f48583200867ac0b1a4
umitobird <umitobird@gmail.com>
2022-03-31 18:26:54 +0800

    0331

--- src/main/java/Test/ticket/Tester.java
+++ src/main/java/Test/ticket/Tester.java
@@ -5,11 +5,14 @@ import java.util.Scanner;
 public class Tester {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Your start station? 1)Taipei 2)Taichung 3)Kouhsiung");
         Station startstation = null;
         Station destination = null;
+        System.out.println("Your startstation? 1)Taipei 2)Taichung 3)Kouhsiung");
         int choice1 = Integer.parseInt(scanner.next());
+        System.out.println("Your destination? 1)Taipei 2)Taichung 3)Kouhsiung");
         int choice2 = Integer.parseInt(scanner.next());
+        System.out.println("Your ticket? 1)Adult 2)Concession");
+        int choice3 = Integer.parseInt(scanner.next());
         switch(choice1){
             case 1:
                 startstation = Station.TAIPEI_STATION;
@@ -30,6 +33,14 @@ public class Tester {
                 break;
             case 3:
                 destination = Station.KAOHSIUNG_STATION;
+                break;
+        }
+        switch(choice3){
+            case 1:
+                destination = Station.TAIPEI_STATION;
+                break;
+            case 2:
+                destination = Station.TAICHUNG_STATION;
                 break;
         }
         Ticket ticket = new Ticket(startstation, destination);
