596d851c7bba660507bffc1ca96682d29aa7dc0f
umitobird <umitobird@gmail.com>
2022-03-31 18:18:37 +0800

    0331

--- src/main/java/Test/ticket/Tester.java
+++ src/main/java/Test/ticket/Tester.java
@@ -4,18 +4,35 @@ import java.util.Scanner;
 
 public class Tester {
     public static void main(String[] args) {
-        Ticket ticket = new Ticket(Station.TAIPEI_STATION, Station.KAOHSIUNG_STATION);
         Scanner scanner = new Scanner(System.in);
         System.out.println("Your start station? 1)Taipei 2)Taichung 3)Kouhsiung");
         Station startstation = null;
-        int choice = Integer.parseInt(Scanner.next());
-        switch(choice){
+        Station destination = null;
+        int choice1 = Integer.parseInt(scanner.next());
+        int choice2 = Integer.parseInt(scanner.next());
+        switch(choice1){
             case 1:
                 startstation = Station.TAIPEI_STATION;
+                break;
             case 2:
                 startstation = Station.TAICHUNG_STATION;
+                break;
             case 3:
                 startstation = Station.KAOHSIUNG_STATION;
+                break;
         }
+        switch(choice2){
+            case 1:
+                destination = Station.TAIPEI_STATION;
+                break;
+            case 2:
+                destination = Station.TAICHUNG_STATION;
+                break;
+            case 3:
+                destination = Station.KAOHSIUNG_STATION;
+                break;
+        }
+        Ticket ticket = new Ticket(startstation, destination);
+        ticket.print();
     }
 }
--- src/main/java/Test/ticket/Ticket.java
+++ src/main/java/Test/ticket/Ticket.java
@@ -4,15 +4,23 @@ public class Ticket {
     public static final int TAIPEI_STATION = 100;
     public static final int TAICHUNG_STATION = 200;
     public static final int KAOHSIUNG_STATION = 300;
-
-    Station start;
-    Station destination;
     int price;
-    public Ticket(Station start, Station destination){
-        this.start = start;
+    Station startstation;
+    Station destination;
+    public Ticket(Station startstation, Station destination){
+        this.startstation = startstation;
         this.destination = destination;
     }
+    public int getPrice() {
+        if (startstation == Station.TAIPEI_STATION ||
+                destination == Station.TAICHUNG_STATION){
+            this.price = 600;
+        }
+        return price;
+    }
     public void print(){
-
+        System.out.println("起始站：" + startstation.id + "\t" + startstation.name
+                + "\n" + "終點站：" + destination.id + "\t" + destination.name
+                + "\n" + "價格："+ getPrice());
     }
 }
