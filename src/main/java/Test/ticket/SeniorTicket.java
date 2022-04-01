0415cf1ef94bcc155f8573257ab567bea6c575a3
umitobird <umitobird@gmail.com>
2022-04-01 16:31:06 +0800

    0401a

--- src/main/java/Test/ticket/SeniorTicket.java
+++ src/main/java/Test/ticket/SeniorTicket.java
@@ -1,8 +1,9 @@
 package Test.ticket;
 
-public class SeniorTicket extends StudentTicket{
+public class SeniorTicket extends Ticket{
+    float off = 0.5f;
     public SeniorTicket(Station startstation, Station destination) {
         super(startstation, destination);
-        float off = 0.5f;
+        price = price-(int)(price*off);
     }
 }
