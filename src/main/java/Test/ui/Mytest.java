54b309e3ee2e28ed50999792d5d6993df146f23a
407570397 <407570397@mail.fju.edu.tw>
2022-06-09 23:51:28 +0800

    0609

--- src/main/java/Test/ui/Mytest.java
+++ src/main/java/Test/ui/Mytest.java
@@ -4,27 +4,41 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Random;
 
-public class Mytest {
+public class Mytest extends JFrame {
+    JButton button = new JButton("GUESS");
+    JLabel label = new JLabel("Please Enter 1~10");
+    JTextField number = new JTextField(5);
+    Random random = new Random();
+    int secret =random.nextInt(10)+1;
     public static void main(String[] args) {
-        JFrame frame = new JFrame();
-        frame.setSize(300,200);
-        frame.setLocation(0,0);
-        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        JButton button = new JButton("OK");
-//        button.addActionListener(new ActionListener() {
-//            @Override
-//            public void actionPerformed(ActionEvent e) {
-//                System.out.println("HA");
-//            }
-//        });
-        MyActionListener listener = new MyActionListener();
-        button.addActionListener(listener);
-        frame.setLayout(new FlowLayout());
-        frame.add(new JButton("Testing"));
-        frame.add(button);
-
-        frame.setVisible(true);
-        System.out.println("END?");
+        Mytest Mytest = new Mytest();
+    }
+    public Mytest(){
+        super();
+        setSize(600,400);
+        setLocation(300,200);
+        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+        System.out.println("Secret:"+secret);
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int num = Integer.parseInt(number.getText());
+                if (num > secret){
+                    label.setText("Smaller");
+                }else if(num < secret) {
+                    label.setText("Bigger");
+                }else {
+                    label.setText("Bingo,the number is " + secret);
+                    System.out.println("END");
+                }
+            }
+        });
+        setLayout(new FlowLayout());
+        add(number);
+        add(button);
+        add(label);
+        setVisible(true);
     }
 }
